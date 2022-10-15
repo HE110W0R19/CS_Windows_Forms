6af2a6338d2e870946bce32bdf3bd54db9ffca2c
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2022-10-15 22:06:19 +0300

    Home work 4 Vstrecha 5

---
+++ HomeWork4/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+//Задача 1
+
+namespace HomeWork4
+{
+    public partial class Form1 : Form
+    {
+        Form2 form = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (form != null)
+                form.Close();
+            form = new Form2();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                for (int i = 0; i < form.tmp.Count; ++i)
+                {
+                    listBox1.Items.Add(form.tmp[i]);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HomeWork4/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Задача 1
+
+namespace HomeWork4
+{
+    public partial class Form2 : Form
+    {
+        public List<string> tmp = new List<string> { };
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(textBox1.Text.ToString());
+            System.IO.FileInfo[] files = dir.GetFiles(textBox2.Text.ToString());
+            foreach (System.IO.FileInfo fi in files)
+            {
+                tmp.Add(fi.ToString());
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
